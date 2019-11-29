f70b1279b769c850d844dfe0b191d12ddf15c4e1
Jason Magpantay <jasonm@visualrisk.com>
2019-11-29 13:15:35 +0800

    added custom response handler

--- generators/app/templates/server/api/controllers/BaseController.ts
+++ generators/app/templates/server/api/controllers/BaseController.ts
@@ -1,26 +1,25 @@
+import * as _ from 'lodash';
 import { Controller } from 'tsoa';
 import { ErrorInfo, EntityMetadata } from '../models';
 
 export class BaseController extends Controller {
-  sendErrorResponse<TResult>(
-    errorType: string,
-    statusCode: number
-  ): Promise<EntityMetadata<TResult>> {
+  sendErrorResponse<TResult>(errorType: string, statusCode: number): Promise<EntityMetadata<TResult>> {
     this.setStatus(statusCode);
     const error: ErrorInfo = new ErrorInfo(errorType);
     return Promise.resolve(new EntityMetadata<TResult>(null, error));
   }
 
-  handleResponse<TResult>(
-    results: EntityMetadata<TResult>,
-    unhandledError: string
-  ): Promise<EntityMetadata<TResult>> {
+  handleResponse<TResult>(results: EntityMetadata<TResult>, unhandledError: string, onHandleResponse: (result: EntityMetadata<TResult>) => void): Promise<EntityMetadata<TResult>> {
     if (!results) {
       return this.sendErrorResponse(unhandledError, 500);
     }
 
     if (results.error) {
       return this.sendErrorResponse(results.error.errorType, 500);
+    }
+
+    if (_.isFunction(onHandleResponse) && !_.isNil(onHandleResponse)) {
+      onHandleResponse(results);
     }
 
     this.setStatus(200);
